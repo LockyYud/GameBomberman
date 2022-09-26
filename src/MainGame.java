6a2db2674d7718c5d62a5c37aadc50a7ef93219c
DoMinhDuy21020292 <100185927+DoMinhDuy21020292@users.noreply.github.com>
2022-09-26 17:31:39 +0700

    update main

--- src/MainGame.java
+++ src/MainGame.java
@@ -1,9 +1,11 @@
+import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.shape.Circle;
@@ -18,15 +20,24 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.text.FontWeight;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.util.Duration;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.security.Key;
 import java.util.concurrent.TransferQueue;
-public class MainGame extends Application {
+public class MainGame extends Application implements LoadImage{
     public static final int window_height = 600;
     public static final int window_width = 600;
-    public static final int size_of_box = 50;
+    int i = 0;
+    static int x = 50;
+    static int y = 50;
     @Override
     public void start(Stage stage) {
-
+        Bomer player = new Bomer();
+        Scene scene = new Scene(player.move, 600 , 600);
+        scene.addEventHandler(KeyEvent.KEY_PRESSED ,player.handler);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String args[]){
